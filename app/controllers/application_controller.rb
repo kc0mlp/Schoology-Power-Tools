a852cadfe56244621311396f3a9019d2f7393482
kc0mlp <kc0mlp@gmail.com>
2015-09-25 17:00:05 -0500

    Completed devise setup, added migration for admin attribute. Working on flash alerts for admin only view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Checks for users admin status
+  def ensure_admin!
+    unless user_signed_in?
+      redirect_to root_path
+      return false
+    end
+    unless current_user.admin?
+
+      redirect_to welcome_adminonly_path
+
+      return false
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+
+  def flash_class(level)
+    case level
+      when :notice then return "alert alert-info"
+      when :success then return "alert alert-success"
+      when :error then return "alert alert-danger"
+      when :alert then return "alert alert-error"
+    end
+
+  end
 end

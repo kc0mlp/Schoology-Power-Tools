0bbca50496590e7b78b7865c6103344729578f35
kc0mlp <kc0mlp@gmail.com>
2015-09-28 18:05:01 -0500

    Moved on to grade search tool.

--- app/controllers/grade_reports_controller.rb
+++ app/controllers/grade_reports_controller.rb
@@ -2,5 +2,20 @@ class GradeReportsController < ApplicationController
   before_filter :ensure_admin!
 
   def generate_report
+
+
+
+    # Setup Oauth consumer info for header
+    # ToDo: Convert this to environment config
+    consumer = OAuth::Consumer.new('6bef9cd9f459440b429c1d9186c180d5055aeb4b9', '6dbf4181976a60b0ea93a61e48fcb99f',
+                                   {
+                                       :site => 'https://api.schoology.com/v1',
+                                       :scheme => :header,
+                                       :http_method => :get
+                                   })
+    resp = consumer.request(:get, '/messages/inbox')
+
+    @data = resp.body
+
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,6 @@
 class WelcomeController < ApplicationController
   #before_filter :authenticate_user!
   def index
-    # Setup Oauth consumer info for header
-    # ToDo: Convert this to environment config
-    consumer = OAuth::Consumer.new('6bef9cd9f459440b429c1d9186c180d5055aeb4b9', '6dbf4181976a60b0ea93a61e48fcb99f',
-                                   {
-                                       :site => 'https://api.schoology.com/v1',
-                                       :scheme => :header,
-                                       :http_method => :get
-                                   })
-    resp = consumer.request(:get, '/messages/inbox')
-
-    @data = resp.body
 
 
   end
